54637a914b8f5dff6459c27b7b92451e35865f00
xghui <jxronghui@gmail.com>
2020-03-11 19:18:02 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,6 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_generate
+git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
+git clone https://github.com/ntlf9t/luci-app-dnspod.git package/luci-app-dnspod
