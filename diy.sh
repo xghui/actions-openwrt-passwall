177053489f9773f7ee11450cf2f97c4a46026d53
xghui <jxronghui@gmail.com>
2020-03-17 23:36:50 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,3 +9,4 @@
 sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_generate
 git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
 git clone https://github.com/ntlf9t/luci-app-dnspod.git package/luci-app-dnspod
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
